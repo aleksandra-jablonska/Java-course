81f4b5bedd754623008d4fd6f4618d2ac2eeeadd
Aleksandra Jabłońska <aleksandra.o.jablonska@gmail.com>
2023-02-24 11:31:16 +0100

    working on file app.js. File is modified

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let activeSlideNumber = 1;
+
 let arrowLeft = document.querySelector('.arrow-left');
 let arrowRight = document.querySelector('.arrow-right');
 
@@ -9,4 +11,35 @@ let slide1 = document.querySelector('#slide1');
 let slide2 = document.querySelector('#slide2');
 let slide3 = document.querySelector('#slide3');
 
-dot2.addEventListener('click');
+let hideActiveSlide = () => {
+    let activeElement = document.querySelector('.active');
+    activeElement.classList.remove('active');
+};
+
+let showSlide = (slideNumber) => {
+    hideActiveSlide();
+    document.querySelector('#slide'+slideNumber).classList.add('active');
+}
+
+let showNextSlide = () => {
+    activeSlideNumber = activeSlideNumber + 1;
+    showSlide(activeSlideNumber);
+}
+
+let showSlide1 = () => {
+    showSlide(1);
+};
+
+let showSlide2 = () => {
+    showSlide(2);
+};
+
+let showSlide3 = () => {
+    showSlide(3);
+};
+
+dot1.addEventListener('click', showSlide1);
+dot2.addEventListener('click', showSlide2);
+dot3.addEventListener('click', showSlide3);
+
+arrowRight.addEventListener('click', showNextSlide);
